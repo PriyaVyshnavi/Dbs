c63643472a754598141565875ed233c093fb8dad
PriyaVyshnavi <39427163+PriyaVyshnavi@users.noreply.github.com>
2018-05-19 15:54:42 +0530

    login page

--- app/src/main/java/com/example/kumar/dbs/MainActivity.java
+++ app/src/main/java/com/example/kumar/dbs/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.kumar.dbs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static EditText emailid;
+    public static EditText password;
+    public Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
